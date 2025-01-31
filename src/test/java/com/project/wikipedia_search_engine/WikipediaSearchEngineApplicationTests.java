e19a8064894ac903a50f881a7cb3612d183870da
Sunny <sunny@Sunnys-MacBook-Air.local>
2025-01-31 16:30:52 +0530

    initialised-repository

---
+++ src/test/java/com/project/wikipedia_search_engine/WikipediaSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wikipedia_search_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikipediaSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
